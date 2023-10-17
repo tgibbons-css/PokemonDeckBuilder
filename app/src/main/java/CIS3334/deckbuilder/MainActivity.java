52e1c4c21317be67f4c719e430d4a56bc33d04f5
PhoenixVerus <bphelwig@gmail.com>
2023-10-17 17:58:46 -0500

    Initial commit

---
+++ app/src/main/java/CIS3334/deckbuilder/MainActivity.java
@@ -0,0 +1,14 @@
+package CIS3334.deckbuilder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
